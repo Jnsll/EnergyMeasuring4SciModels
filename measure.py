932b3b55dbd66e6bcdd757818e07d37b8a7893c7
Jnsll <j.sallou@tudelft.nl>
2024-01-30 11:57:44 +0100

    add managestion of file names in csv file as parameter

--- measure.py
+++ measure.py
@@ -1,6 +1,12 @@
 import subprocess
 import sys
+import pandas as pd
 
-script_command = ["/home/tdurieux/git/EnergiBridge/target/release/energibridge" ,"--summary" ,"--output", "./output/energy_metrics.csv" ,"-c" ,"./output/output_simulation.txt" ,"docker" ,"run", "--rm", "-v", "./scripts:/scripts" ,"-v" ,"./matlab.dat:/licenses/license.lic", "-e", "MLM_LICENSE_FILE=/licenses/license.lic", "mathworks/matlab:r2021b" ,"-batch", "run('/scripts/candidate_script.m');exit();"]
 
-result = subprocess.run(script_command)
+scripts_to_run_df = pd.read_csv('sci_script.csv')
+for ind in scripts_to_run_df.index:
+    sci_script = scripts_to_run_df['Paths'][ind]
+
+    script_command = ["/home/tdurieux/git/EnergiBridge/target/release/energibridge" ,"--summary" ,"--output", "./output/energy_metrics.csv" ,"-c" ,"./output/output_simulation.txt" ,"docker" ,"run", "--rm", "-v", "./scripts:/scripts" ,"-v" ,"./matlab.dat:/licenses/license.lic", "-e", "MLM_LICENSE_FILE=/licenses/license.lic", "mathworks/matlab:r2021b" ,"-batch", "run('" + str(sci_script) + "');exit();"]
+
+    result = subprocess.run(script_command)
