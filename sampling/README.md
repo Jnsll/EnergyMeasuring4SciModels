94d8149b028cfca7cafbe515eec9cd6c204fee5e
Alexander Boll <boll@informatik.hu-berlin.de>
2024-02-15 15:41:02 +0100

    refiner.py is working in v1

---
+++ sampling/README.md
@@ -0,0 +1,5 @@
+# Usage
+
+
+Call each python program like this `python <YOUR GITHUB LOGIN NAME> <YOUR GITHUB TOKEN>`.
+Call `sampler.py` (maybe stop at some point so to not get too many projects), then `sampler.py`.
--- sampling/refiner.py
+++ sampling/refiner.py
@@ -1,7 +1,89 @@
-import os.path
+import numpy as np
 import pandas as pd
+import os
+import sys
+import time
+import requests
+from datetime import datetime
 
-list_path = f"./projects_Matlab.csv"
-project_list = pd.read_csv(list_path)
 
-print(project_list)
+# call this function with parameters <YOUR GITHUB LOGIN NAME> <YOUR GITHUB TOKEN>
+
+def sleeep(x):
+    print("Search failed: ", x.json()["message"], " Waiting 30s ...")
+    time.sleep(30)
+
+
+def API_commit_URL(name):
+    return f"https://api.github.com/repos/{name}/commits"
+
+
+def API_repo_URL(name):
+    return f"https://api.github.com/repos/{name}"
+
+
+def str_to_datetime(dt_str):
+    return datetime.strptime(dt_str, '%Y-%m-%dT%H:%M:%SZ')
+
+
+def analyze_commits(commits):
+    first_commit, last_commit = datetime(2025, 1, 1), datetime(1970, 1, 1)
+    authors = set()
+    for commit in commits:
+        try:
+            authors.add(commit["author"]["id"])
+        except:
+            try:
+                authors.add(commit["commit"]["author"]["name"])
+            except:
+                pass  # corrupted commit?
+        date = str_to_datetime(commit["commit"]["author"]["date"])
+        if date > last_commit:
+            last_commit = date
+        if date < first_commit:
+            first_commit = date
+
+    return len(commits), first_commit.isoformat(), last_commit.isoformat(), (
+            last_commit - first_commit).total_seconds() / 86400, len(authors)
+
+
+list_path = "projects_Matlab.csv"
+list_path_refined = "projects_Matlab_refined.csv"
+
+if not os.path.isfile(list_path_refined):
+    project_list = pd.read_csv(list_path).sample(frac=1).reset_index(drop=True)
+
+    project_list["#commits"] = -1
+    project_list["first commit"] = ".."
+    project_list["last commit"] = ".."
+    project_list["project life time (s)"] = -1
+    project_list["#contributors"] = -1
+    project_list["stars"] = -1
+    project_list["online"] = True
+    project_list["included"] = True
+
+else:
+    project_list = pd.read_csv(list_path_refined)
+
+for i in range(len(project_list)):
+    if project_list.loc[i, "#commits"] != -1 or project_list.loc[i, "included"] is False:
+        continue
+    print(i, project_list.loc[i, "name"])
+    tries = 0
+    while True:
+        tries += 1
+        commits = requests.get(API_commit_URL(project_list.iloc[i]["name"]), auth=(sys.argv[1], sys.argv[2]))
+        repo = requests.get(API_repo_URL(project_list.iloc[i]["name"]), auth=(sys.argv[1], sys.argv[2]))
+        if commits.ok and repo.ok or tries > 10:
+            break
+        sleeep(commits)
+    if tries > 10:
+        project_list.loc[i, ["online", "included"]] = (False, False)
+        continue
+    commits, repo = commits.json(), repo.json()
+    project_list.loc[
+        i, ["#commits", "first commit", "last commit", "project life time (s)", "#contributors"]] = analyze_commits(
+        commits)
+    project_list.loc[i, "stars"] = repo["stargazers_count"]
+    if i % 100 == 0:
+        project_list.to_csv(list_path_refined, index=False)
