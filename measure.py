4e255a2fd3ee97bdfb5d9e5eb4025ba5ad1f532f
Jnsll <j.sallou@tudelft.nl>
2024-02-26 18:15:55 +0100

    fix args + baseline exps

--- measure.py
+++ measure.py
@@ -14,42 +14,45 @@ import argparse
 ### Experimental Parameters
 random.seed(42)
 repetition_number = 30
-BASELINE = args.baseline
 
 #sci_script = "/scripts/deep-photo-styletransfer/gen_laplacian/gen_laplacian.m"
 
-### Extracting scripts of Matlab projects to run
-with open("test.csv", "r") as file:
-    lines = file.read().splitlines()
 
-### Exerimentation set up
-## Repetition
-scripts_executions = lines * repetition_number
-## Shuffle of Matlab project to run
-random.shuffle(scripts_executions)
-print(scripts_executions)
+def run_experiments_matlab(repetition_number, baseline):
+    if baseline:
+        for count in range(1,repetition_number+1):
+            script_command = ["/home/tdurieux/git/EnergiBridge/target/release/energibridge" ,"--summary" ,"--output", "./output/energy_metrics_" + str(count) + ".csv" ,"-c" ,"./output/output_simulation_"+ str(count) + ".txt" ,"docker" ,"run", "--rm", "-v", "./scripts:/scripts" ,"-v" ,"./matlab.dat:/licenses/license.lic", "-e", "MLM_LICENSE_FILE=/licenses/license.lic", "matlab-r2021b-toolbox" ,"-batch", "exit();"]
+            result = subprocess.run(script_command)
+    else:
+        ### Extracting scripts of Matlab projects to run
+        with open("test.csv", "r") as file:
+            lines = file.read().splitlines()
 
-### Running experiment executions
-print(BASELINE)
-count = 0
-if BASELINE is False:
-    for execution in scripts_executions:
-        count += 1
-        script_command = ["/home/tdurieux/git/EnergiBridge/target/release/energibridge" ,"--summary" ,"--output", "./output/energy_metrics_" + str(count) + ".csv" ,"-c" ,"./output/output_simulation_"+ str(count) + ".txt" ,"docker" ,"run", "--rm", "-v", "./scripts:/scripts" ,"-v" ,"./matlab.dat:/licenses/license.lic", "-e", "MLM_LICENSE_FILE=/licenses/license.lic", "matlab-r2021b-toolbox" ,"-batch", "run('" + str(execution) + "');exit();"]
-        result = subprocess.run(script_command)
-else:
-    for count in range(1,repetition_number+1):
-        script_command = ["/home/tdurieux/git/EnergiBridge/target/release/energibridge" ,"--summary" ,"--output", "./output/energy_metrics_" + str(count) + ".csv" ,"-c" ,"./output/output_simulation_"+ str(count) + ".txt" ,"docker" ,"run", "--rm", "-v", "./scripts:/scripts" ,"-v" ,"./matlab.dat:/licenses/license.lic", "-e", "MLM_LICENSE_FILE=/licenses/license.lic", "matlab-r2021b-toolbox" ,"-batch", "exit();"]
-        result = subprocess.run(script_command)
+        ### Exerimentation set up
+        ## Repetition
+        scripts_executions = lines * repetition_number
+        ## Shuffle of Matlab project to run
+        random.shuffle(scripts_executions)
+        print(scripts_executions)
+
+        ### Running experiment executions
+        count = 0
+        for execution in scripts_executions:
+            count += 1
+            script_command = ["/home/tdurieux/git/EnergiBridge/target/release/energibridge" ,"--summary" ,"--output", "./output/energy_metrics_" + str(count) + ".csv" ,"-c" ,"./output/output_simulation_"+ str(count) + ".txt" ,"docker" ,"run", "--rm", "-v", "./scripts:/scripts" ,"-v" ,"./matlab.dat:/licenses/license.lic", "-e", "MLM_LICENSE_FILE=/licenses/license.lic", "matlab-r2021b-toolbox" ,"-batch", "run('" + str(execution) + "');exit();"]
+            result = subprocess.run(script_command)
+        
 
 
 
 
 
-#if __name__ == "__main__":
-parser = argparse.ArgumentParser()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
 
-#parser.add_argument('-rep', '--repetitions')      # option that takes a value
-parser.add_argument('-base', '--baseline',
-                    action='store_true')
-args = parser.parse_args()
+    #parser.add_argument('-rep', '--repetitions')      # option that takes a value
+    parser.add_argument('-base', '--baseline',
+                        action='store_true')
+    args = parser.parse_args()
+    run_experiments_matlab(repetition_number, args.baseline)
+
