61e79896271df2f0e21c1bfdb820794bf508a819
Jnsll <j.sallou@tudelft.nl>
2024-03-27 14:28:20 +0100

    print execution

--- measure.py
+++ measure.py
@@ -74,6 +74,7 @@ def run_experiments_matlab(input_file, repetition_number):
     ### Running experiment executions
     count = 0
     for execution in scripts_executions:
+        print("Execution:", execution)
         count += 1
         script_command = ["/home/tdurieux/git/EnergiBridge/target/release/energibridge" ,"--summary" ,"--output", "./output/energy_metrics_" + str(count) + ".csv" ,"-c" ,"./output/output_simulation_"+ str(count) + ".txt" ,"docker" ,"run", "--rm", "-v", "./sampling:/sampling" , "-v", "./time_study:/time_study", "-v", "./output:/output", "-v" ,"./matlab.dat:/licenses/license.lic", "-e", "MLM_LICENSE_FILE=/licenses/license.lic", "matlab-r2021b-toolbox" ,"-batch", "run('" + str(execution) + "');exit();"]
         start = time.time()
