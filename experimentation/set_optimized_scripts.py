c3399ca4c5996651e6aaf0413b436040e822c583
Jnsll <j.sallou@tudelft.nl>
2024-07-01 13:40:02 +0200

    automation of creation of files with scripts to run for experimentation

--- experimentation/set_optimized_scripts.py
+++ experimentation/set_optimized_scripts.py
@@ -6,6 +6,18 @@ from pathlib import Path
 regex_origin = re.compile('./../resource(.*/)(.*m)')
 regex_optimized = re.compile('./../resource(.*m)')
 
+
+## TO DO
+# create file with scripts to run for experimentS
+
+def create_file_for_experimentaton(list_script_paths):
+    list_scripts_format =  "\n".join(list_script_paths)
+    with open("Experimentation_scripts.csv", "w") as file:
+            file.write(list_scripts_format)
+
+
+
+scripts_to_run_list = []
 with open("../GPT/resource/Optimzation_results/optimized_gpt3/OptimizedMatlabScripts_reasonings.csv", "r") as csvfile: 
     reader_variable = csv.reader(csvfile)
     for row in reader_variable:
@@ -19,21 +31,25 @@ with open("../GPT/resource/Optimzation_results/optimized_gpt3/OptimizedMatlabScr
             name_original_script = p.group(2)
             #print("Orginial script:", path_to_original_script, name_original_script)
         if p_optimized:
-            optimized_script = p_optimized.group(1)
+            path_optimized_script = regex_origin.group(1)
+            name_optimized_script = p_optimized.group(2)
             #print("optimized:", optimized_script)
         try:
             #3print("../GPT/resource" + str(optimized_script))
             #print(Path(".." + str(path_to_original_script) + "/") )
             if p:
-                print("../GPT/resource" + str(optimized_script), ".." + str(path_to_original_script))
-                shutil.copy("../GPT/resource" + str(optimized_script), ".." + str(path_to_original_script))
+                print("../GPT/resource" + str(path_optimized_script) + str(name_optimized_script), ".." + str(path_to_original_script))
+                shutil.copy("../GPT/resource" + str(path_optimized_script) + str(name_optimized_script), ".." + str(path_to_original_script))
+                scripts_to_run_list.append(str(path_to_original_script) + str(name_optimized_script))
                      
         except:
         #    print("Error with the file copy!")
              
             #print(path_to_original_script, name_original_script)
             #print("optimized:", optimized_script)
-            print("error")
+            print("error for", "../GPT/resource" + str(optimized_script))
+
+create_file_for_experimentaton(scripts_to_run_list)
 
 
 
