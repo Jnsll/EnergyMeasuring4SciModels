1725b2d7a1f8c3a2438fd32c69162fbc742b3734
Jnsll <j.sallou@tudelft.nl>
2024-07-01 16:07:55 +0200

    upd cp files for all optimized llms

--- experimentation/set_optimized_scripts.py
+++ experimentation/set_optimized_scripts.py
@@ -15,50 +15,44 @@ def create_file_for_experimentaton(list_script_paths):
     with open("Experimentation_scripts.csv", "w") as file:
             file.write(list_scripts_format)
 
-
+FILES = ["../GPT/resource/Optimzation_results/optimized_gpt3/OptimizedMatlabScripts_reasonings.csv", "../GPT/resource/Optimzation_results/optimized_gpt4/OptimizedMatlabScripts_reasonings.csv", "../GPT/resource/Optimzation_results/optimized_llama/OptimizedMatlabScripts_reasonings.csv", "../GPT/resource/Optimzation_results/optimized_mixtral/OptimizedMatlabScripts_reasonings.csv"]
 
 scripts_to_run_list = []
-with open("../GPT/resource/Optimzation_results/optimized_gpt3/OptimizedMatlabScripts_reasonings.csv", "r") as csvfile: 
-    reader_variable = csv.reader(csvfile)
-    for row in reader_variable:
-        origin_script = row[0]
-        optimized_script = row[1]
-        #print(origin_script)
-        p = re.match(regex_origin, origin_script)
-        p_optimized = re.match(regex_optimized, optimized_script)
-        if p:
-            path_to_original_script = p.group(1)
-            name_original_script = p.group(2)
-            #print("Orginial script:", path_to_original_script, name_original_script)
-        if p_optimized:
-            path_optimized_script = regex_origin.group(1)
-            name_optimized_script = p_optimized.group(2)
-            #print("optimized:", optimized_script)
-        try:
-            #3print("../GPT/resource" + str(optimized_script))
-            #print(Path(".." + str(path_to_original_script) + "/") )
+for file in FILES:
+    with open("../GPT/resource/Optimzation_results/optimized_gpt3/OptimizedMatlabScripts_reasonings.csv", "r") as csvfile: 
+        reader_variable = csv.reader(csvfile)
+        for row in reader_variable:
+            origin_script = row[0]
+            optimized_script = row[1]
+            #print(origin_script)
+            p = re.match(regex_origin, origin_script)
+            p_optimized = re.match(regex_origin, optimized_script)
             if p:
-                print("../GPT/resource" + str(path_optimized_script) + str(name_optimized_script), ".." + str(path_to_original_script))
-                shutil.copy("../GPT/resource" + str(path_optimized_script) + str(name_optimized_script), ".." + str(path_to_original_script))
-                scripts_to_run_list.append(str(path_to_original_script) + str(name_optimized_script))
-                     
-        except:
-        #    print("Error with the file copy!")
-             
-            #print(path_to_original_script, name_original_script)
-            #print("optimized:", optimized_script)
-            print("error for", "../GPT/resource" + str(optimized_script))
+                path_to_original_script = p.group(1)
+                name_original_script = p.group(2)
+                #print("Orginial script:", path_to_original_script, name_original_script)
+            if p_optimized:
+                path_optimized_script = p_optimized.group(1)
+                name_optimized_script = p_optimized.group(2)
+                #print("optimized:", optimized_script)
+            try:
+                #3print("../GPT/resource" + str(optimized_script))
+                #print(Path(".." + str(path_to_original_script) + "/") )
+                if p:
+                    print("../GPT/resource" + str(path_optimized_script) + str(name_optimized_script), ".." + str(path_to_original_script))
+                    shutil.copy("../GPT/resource" + str(path_optimized_script) + str(name_optimized_script), ".." + str(path_to_original_script))
+                    scripts_to_run_list.append(str(path_to_original_script) + str(name_optimized_script))
+                    scripts_to_run_list.append(str(path_to_original_script) + str(name_original_script))
+                        
+            except:
+            #    print("Error with the file copy!")
+                
+                #print(path_to_original_script, name_original_script)
+                #print("optimized:", optimized_script)
+                print("error for", "../GPT/resource" + str(optimized_script))
 
 create_file_for_experimentaton(scripts_to_run_list)
 
 
-
-    
-
-#    print(second)
-    
-    
-#    for row in reader_variable:
-#        print(row[0])
 
 
