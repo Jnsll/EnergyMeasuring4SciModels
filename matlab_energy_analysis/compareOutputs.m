69690482916d31d105d46bad34e2b252de3c0b7a
Alexander Boll <alexander.boll@unibe.ch>
2024-07-22 21:12:42 +0200

    compared outputs of optimized scripts

--- matlab_energy_analysis/compareOutputs.m
+++ matlab_energy_analysis/compareOutputs.m
@@ -1,19 +1,26 @@
 function compareOutputs
-    %csvFiles = {'TmpScripts.csv','OptimizedMatlabScripts_gpt3.csv' 'OptimizedMatlabScripts_gpt4.csv' 'OptimizedMatlabScripts_llama.csv' 'OptimizedMatlabScripts_mixtral.csv'};
-    csvFiles = {'OptimizedMatlabScripts_gpt3.csv' 'OptimizedMatlabScripts_gpt4.csv' 'OptimizedMatlabScripts_llama.csv' 'OptimizedMatlabScripts_mixtral.csv'};
+    clear all;
+    %csvFiles = {'TmpScripts.csv'};
+    csvFiles = {'OptimizedMatlabScripts_gpt3.csv' 'OptimizedMatlabScripts_gpt4.csv' 'OptimizedMatlabScripts_llama.csv' 'OptimizedMatlabScripts_mixtral.csv', 'OptimizedMatlabScripts_human.csv', 'OptimizedMatlabScripts_original.csv'};
     resultCsvFile = 'compareOutput.csv';
     resultCsv = readtable(resultCsvFile);
-
+    
     for c = 1:length(csvFiles)
         csvFile = csvFiles{c};
         csvData = readtable(csvFile);
+
+        shortCSVFile = split(csvFile, '_');
+        shortCSVFile = shortCSVFile{2};
+        shortCSVFile = shortCSVFile(1:end-4);
+
         for r = 1:height(csvData)
-            %skip if we tested thialready tested
-            if any(strcmp(resultCsv.csvFile, csvFile) & (resultCsv.rowNumber == r))
+
+            %skip if we tested the script already
+            if any(strcmp(resultCsv.csvFile, shortCSVFile) & (resultCsv.rowNumber == r))
                 continue
             end
 
-            fprintf("Coming up: %s, row %i\n", csvFile, r)
+            fprintf("Coming up: %s, row %i\n", shortCSVFile, r)
 
             row = struct;
             row.Original_code = char(table2cell(csvData(r,3)));
@@ -27,15 +34,16 @@ function compareOutputs
             rel_script_path = char(table2cell(csvData(r, 1)));
             rel_script_path = rel_script_path(13+length(repo_path):end);
             rel_script_path = strsplit(rel_script_path, '/');
+            short_rel_path = csvData{r, 1}{1}(60:end);
             row.Rel_script_path = strjoin(rel_script_path(1:end-1), '/');
             row.Script_name = rel_script_path{end};
             row.Script_name = row.Script_name(1:end-2);
 
 
-            [same_outputs, runTime, runTimeAI] = two_scripts_are_same(row.RepoPath, row.Rel_script_path, row.Script_name, row.Original_code, row.AI_code);
+            [same_outputs, same_data, same_results, runTime, runTimeAI] = two_scripts_are_same(row.RepoPath, row.Rel_script_path, row.Script_name, row.Original_code, row.AI_code);
 
 
-            new_data = cell2table({csvFile, r, same_outputs, runTime, runTimeAI}, 'VariableNames', resultCsv.Properties.VariableNames);
+            new_data = cell2table({shortCSVFile, r, same_outputs, same_data, same_results, runTime, runTimeAI, short_rel_path}, 'VariableNames', resultCsv.Properties.VariableNames);
             resultCsv = [resultCsv; new_data];
             writetable(resultCsv, resultCsvFile);
         end
--- matlab_energy_analysis/two_scripts_are_same.m
+++ matlab_energy_analysis/two_scripts_are_same.m
@@ -2,45 +2,55 @@
 %
 %output is 1/True IFF both scripts output the same on the console and the
 %repository directory is identical after script execution
-function [same_outputs, runTime, runTimeAI] = two_scripts_are_same(root_path, rel_script_path, script_name, content, contentAI)
-    max_time = 600;
+function [same_outputs, same_data, same_results, runTime, runTimeAI] = two_scripts_are_same(root_path, rel_script_path, script_name, content, contentAI)
 
     try
         fprintf('Testing original version of %s\n', script_name)
-        [hashbefore, console_output, hashafter, runTime] = capture_outputs(root_path, rel_script_path, script_name, content, max_time);
-        fprintf('Testing AI version of %s\n', script_name)
-        [hashbeforeAI, console_outputAI, hashafterAI, runTimeAI] = capture_outputs(root_path, rel_script_path, script_name, contentAI, max_time);
-    
-        if ~strcmp(hashbefore, hashbeforeAI)
-            error("Error occured during copying temporary copies of the directories.")
+        [res, output, hashafter, runTime] = capture_outputs(root_path, rel_script_path, script_name, content);
+        
+        if strcmp(output, 'evalcERROR')
+            [same_outputs, same_data, same_results, runTime, runTimeAI] = outputOrigCrashed(res, runTime);
+            return
         end
 
-        same_outputs = strcmp(console_output, console_outputAI) && strcmp(hashafter, hashafterAI);
+
+        fprintf('Testing AI version of %s\n', script_name)
+        [resAI, outputAI, hashafterAI, runTimeAI] = capture_outputs(root_path, rel_script_path, script_name, contentAI);
+
+
+
+        %same_outputs = isequal(output, outputAI) && strcmp(hashafter, hashafterAI) && isequal(res, resAI);
+        same_outputs = isequal(output, outputAI);
+        same_data = strcmp(hashafter, hashafterAI);
+        same_results = isequal(res, resAI);
     catch ME
-         disp(ME)
+        disp(ME)
+        [same_outputs, runTime, runTimeAI] = outputERROR(ME);
         try
             %cleanup if one script produced errors
             rmdir([root_path '_copy'], 's');
         catch
         end
-    end
-    %produce human readable console output
-    if same_outputs
-        disp("Both scripts produced the SAME console outputs and output files.")
-    else
-        if ~strcmp(console_output, console_outputAI)
-            if strcmp(hashafter, hashafterAI)
-                disp("The scripts DIVERGE in console outputs!")
-            else
-                disp("The scripts DIVERGE in console outputs and output files!")
-            end
-        else
-            disp("The scripts DIVERGE in output files!")
-        end
+        return
     end
 end
 
-function [hash1, console_output, hash2, runTime] = capture_outputs(root_path, rel_script_path, script_name, content, max_time)
+function [same_outputs, same_data, same_results, runTime, runTimeAI] = outputOrigCrashed(res, runTime)
+    same_outputs = -1;
+    same_data = -1;
+    same_results = -1;
+    runTimeAI = -1;
+end
+
+function [same_outputs, same_data, same_results, runTime, runTimeAI] = outputERROR(ME)
+    same_outputs = -2;
+    same_data = -2;
+    same_results = -2;
+    runTime = -2;
+    runTimeAI = -2;
+end
+
+function [res, out, hash_after, runTime] = capture_outputs(root_path, rel_script_path, script_name, content)
 
     % Create a temporary directory for the copy
     temp_dir = [root_path '_copy'];
@@ -52,9 +62,6 @@ function [hash1, console_output, hash2, runTime] = capture_outputs(root_path, re
     % Copy the root_path directory to temp_dir
     copyfile(root_path, temp_dir, 'f');
 
-    % Compute the initial hash of the directory
-    hash1 = hash_directory(temp_dir);
-
     %copy the content into the script file
     script_file_path = [temp_dir filesep rel_script_path filesep script_name '.m'];
     fid = fopen(script_file_path, 'w');
@@ -62,23 +69,70 @@ function [hash1, console_output, hash2, runTime] = capture_outputs(root_path, re
         error('File could not be opened for writing.');
     end
     fprintf(fid, '%s', ['%TMP SCRIPT FOR TESTING OUTPUTS' newline newline content]);
+    
     fclose(fid);
 
     % Capture the output of the script
-    execution_path = [temp_dir filesep rel_script_path];
-    [console_output, runTime] = start_and_monitor(max_time, execution_path, script_name);
+    tic
+    [res, out] = my_eval([temp_dir filesep rel_script_path], script_name);
+    runTime = toc;
+
+    if strcmp(res, 'MATLAB:scriptNotAFunction')
+        disp("Functionifying ...")
+        functionify(script_file_path, script_name, content)
+        tic
+        [res, out] = my_eval([temp_dir filesep rel_script_path], script_name);
+        runTime = toc;
+    end
+
 
     % Compute the final hash of the directory
     delete(script_file_path);
-    hash2 = hash_directory(temp_dir);
+    hash_after = hash_directory(temp_dir);
 
     % Clean up: remove the temporary directory
-    try
-        rmdir(temp_dir, 's');
-    catch
-        pause(3)
-        rmdir(temp_dir, 's');
+    opened = openedFiles();
+    for i=1:length(opened)
+        fclose(opened(i))
     end
+    rmdir(temp_dir, 's');
+end
+
+function functionify(script_file_path, script_name, content)
+    fid = fopen(script_file_path, 'w');
+    if fid == -1
+        error('File could not be opened for writing.');
+    end
+    fprintf(fid, '%s', ['%TMP SCRIPT FOR TESTING OUTPUTS' newline 'function out = ' script_name '()' newline content newline 'out = '''';' newline 'variables=whos; for i = 1:length(variables); varName = variables(i).name; varValue = eval(varName); fprintf(''Variable: %s\n'', varName); disp(varValue); fprintf(''\n'');' newline 'end']);
+    fclose(fid);
+end
+
+
+
+function [res, out] = my_eval(script_path, script_name)
+    save('preScriptWorkspace.mat');
+    cd(script_path)
+    try
+        [res, out] = evalc(script_name);
+        disp("eval c completed")
+    catch ME
+        res = ME.identifier;
+        out = 'evalcERROR';
+        try
+            %try again without expecting an output
+            res = evalc(script_name);
+            out = 'empty_output';
+        catch ME
+            res = ME.identifier;
+            out = 'evalcERROR';
+        end
+    end
+    close all;
+    cd('C:\work\EnergyMeasuring4SciModels\matlab_energy_analysis')
+    load('preScriptWorkspace.mat');
+    save('preScriptWorkspace.mat');
+    clear all;
+    load('preScriptWorkspace.mat');
 end
 
 function hash = hash_directory(directory)
